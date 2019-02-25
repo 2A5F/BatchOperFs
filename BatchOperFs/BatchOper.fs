8060c447b42acde67c9e2c27355462a81213c36d
2A5F <Aditrc@outlook.com>
2019-02-25 18:37:04 +0800

    add doc

--- BatchOperFs/BatchOper.fs
+++ BatchOperFs/BatchOper.fs
@@ -1,50 +1,72 @@
 module BatchOper
 
-type OrBuilder() =
+type BatchOrBuilder() =
     member __.Yield _ = fun c x -> c x
 
     member __.Run f = fun x -> (f <| fun _ -> false) x
 
     [<CustomOperation("eq", AllowIntoPattern = true)>]
+    /// Equal
     member __.Equals(f, [<ProjectionParameter>] a) = fun c -> f <| fun x -> a() = x || c x
 
     [<CustomOperation("ne", AllowIntoPattern = true)>]
+    /// Not equal
     member __.NotEquals(f, [<ProjectionParameter>] a) = fun c -> f <| fun x -> a() <> x || c x
 
     [<CustomOperation("gt", AllowIntoPattern = true)>]
+    /// Greater than
     member __.GreaterThan(f, [<ProjectionParameter>] a) = fun c -> f <| fun x -> a() > x || c x
 
     [<CustomOperation("lt", AllowIntoPattern = true)>]
+    /// Less than
     member __.LessThan(f, [<ProjectionParameter>] a) = fun c -> f <| fun x -> a() < x || c x
 
     [<CustomOperation("ge", AllowIntoPattern = true)>]
+    /// Greater then or equal
     member __.GreaterEquals(f, [<ProjectionParameter>] a) = fun c -> f <| fun x -> a() >= x || c x
 
     [<CustomOperation("le", AllowIntoPattern = true)>]
+    /// Less then or equal
     member __.LessEquals(f, [<ProjectionParameter>] a) = fun c -> f <| fun x -> a() <= x || c x
 
-type AndBuilder() =
+type BatchAndBuilder() =
     member __.Yield _ = fun c x -> c x
 
     member __.Run f = fun x -> (f <| fun _ -> true) x
 
     [<CustomOperation("eq", AllowIntoPattern = true)>]
+    /// Equal
     member __.Equals(f, [<ProjectionParameter>] a) = fun c -> f <| fun x -> a() = x && c x
 
     [<CustomOperation("ne", AllowIntoPattern = true)>]
+    /// Not equal
     member __.NotEquals(f, [<ProjectionParameter>] a) = fun c -> f <| fun x -> a() <> x && c x
 
     [<CustomOperation("gt", AllowIntoPattern = true)>]
+    /// Greater than
     member __.GreaterThan(f, [<ProjectionParameter>] a) = fun c -> f <| fun x -> a() > x && c x
 
     [<CustomOperation("lt", AllowIntoPattern = true)>]
+    /// Less than
     member __.LessThan(f, [<ProjectionParameter>] a) = fun c -> f <| fun x -> a() < x && c x
 
     [<CustomOperation("ge", AllowIntoPattern = true)>]
+    /// Greater then or equal
     member __.GreaterEquals(f, [<ProjectionParameter>] a) = fun c -> f <| fun x -> a() >= x && c x
 
     [<CustomOperation("le", AllowIntoPattern = true)>]
+    /// Less then or equal
     member __.LessEquals(f, [<ProjectionParameter>] a) = fun c -> f <| fun x -> a() <= x && c x
 
-let batchOr = OrBuilder()
-let batchAnd = AndBuilder()
+/// x |> batchOr { eq a; ne b; gt c; lt d; ge e; le f; }
+///
+/// is equivalent to
+///
+/// x = a || x <> b || x > c || x < d || x >= e || x <= f
+let batchOr = BatchOrBuilder()
+/// x |> batchAnd { eq a; ne b; gt c; lt d; ge e; le f; }
+///
+/// is equivalent to
+///
+/// x = a && x <> b && x > c && x < d && x >= e && x <= f
+let batchAnd = BatchAndBuilder()
